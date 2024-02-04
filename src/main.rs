73256f667ca01e201db4629595817366e70e170f
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-02-04 13:42:32 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Hello Aaan ! This is your first Rust Program")
+}
---
+++ src/third.rs
@@ -0,0 +1,3 @@
+fn main(){
+    println!("Hello Aman  Ready To Go!")
+}git 
